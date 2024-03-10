cd086138a38028652451f959d7b11e3c29654045
Skillet-95 <yfeijoo95@gmail.com>
2024-03-10 16:24:09 -0400

    LH-7 Create Product Showcase component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ShowCase from "./components/containers/showcase";
+import ShowCase from "./components/containers/ShowCase";
 
 const inter = Inter({ subsets: ["latin"] });
 
