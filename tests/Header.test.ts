89e02b95aca0df71c69fd5985206871b8cfecf3f
Bismarck <emiliocardero1@gmail.com>
2024-03-10 21:10:09 -0500

    added virtual dom testing and eslint testing libraries

--- tests/Header.test.ts
+++ tests/Header.test.ts
@@ -1,6 +1,9 @@
-import { describe } from "node:test";
+import { describe, it } from "node:test";
 
 
 describe('testing the header component', () => {
     //we need the component defned first
+    it('should have a logo img', () => {
+        //
+    });
 });
