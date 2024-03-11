c67d39fad2edd1c0af508570dcfca80f318d285d
UciLmgarcia <uci.lmgarcia@gmail.com>
2024-03-11 19:29:07 -0400

    update

--- src/app/components/containers/Header.tsx
+++ src/app/components/containers/Header.tsx
@@ -6,7 +6,7 @@ import { Fragment } from "react";
 
 const Header = () => {
     return (
-        <Popover className={"container mx-auto flex items-center border-b-2 px-6 py-2 h-24"}>
+        <Popover className={"font-custom container mx-auto flex items-center border-b-2 px-6 py-2 h-24 z-999"}>
             <h1 className="font-bold">Page Title</h1>
             <div className="grow">
                 <div className="hidden md:flex items-center justify-center gap-2 md:gap-8">
@@ -18,42 +18,42 @@ const Header = () => {
                 </div>
             </div>
             <div className="flex grow items-center justify-end md:hidden">
-                <Popover.Button className="inline-felx items-center justify-center rounded-md 
-            bg-white p-2 text-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2
-            focus:ring-inset focus:ring-indigo-500">
+                <Popover.Button className="inline-flex items-center justify-center rounded-md 
+                bg-[#fff] p-2 text-[#000] hover:bg-gray-500 focus:outline-none focus:ring-2
+                focus:ring-inset focus:ring-indigo-500 block">
                     <span className="sr-only">
                         Open menu
                     </span>
                     <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                 </Popover.Button>
             </div>
-            <Popover.Overlay className="fixed md:hidden inset-0 bg-black opacity-40" />
+            <Popover.Overlay className="fixed inset-0 bg-black opacity-30" />
             <Transition
                 as={Fragment}
-                enter="duration-200 ease-out"
+                enter="transform duration-300 ease-out transition"
                 enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="duration-100 ease-in"
+                enterTo=" scale-100"
+                leave="transform duration-100 ease-in transition"
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
             >
-                <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2
-                transition md-hidden">
-                    <div className="rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 divid-y-2
+                <Popover.Panel focus className="absolute inset-x-0 top-0 right-0 origin-top-right transform p-2
+                transition md:hidden block opacity-100 z-998">
+                    <div className="rounded-lg bg-[#fff] shadow-lg ring-1  divide-y-2 opacity-100 block
                     divide-gray-50">
-                        <div className="px-5 pt-5 pb-6">
+                        <div className="px-5 pt-5 pb-6 opacity-100">
                             <div className="flex items-center justify-between">
                                 <h1 className="font-bold text-black">Page Title</h1>
                                 <div className="-nr-2">
                                     <Popover.Button className="inline-flex items-center justify-centar rounded-md
-                                        bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none
+                                        bg-[#fff] p-2 text-[#000] hover:bg-gray-100 hover:text-gray-500 focus:outline-none
                                         focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                                         <span className="sr-only">Close menu</span>
-                                        <XMarkIcon className="h-6 w-6" aria-hidden="true"></XMarkIcon>
+                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                                     </Popover.Button>
                                 </div>
                             </div>
-                            <div className="mt-6">
+                            <div className="mt-6 opacity-100">
                                 <nav className="grid gap-y-8">
                                     <Link href="/" className="text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2">Home</Link>
                                     <Link href="/" className="text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2">Home</Link>
@@ -62,13 +62,13 @@ const Header = () => {
                                     <Link href="/" className="text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2">Home</Link>
                                 </nav>
                             </div>
-                            <div className="mt-6 flex flex-col items-center gap-2">
-                                <Link href="register" className="rounded-md bg-white px-4 py-2 text-sm font-medium text-black md:text-xl w-full border-2
-                    focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
+                            <div className="mt-6 flex flex-col items-center gap-2 opacity-100">
+                                <Link href="register" className="rounded-md bg-[#fff] px-4 py-2 text-sm font-medium text-black md:text-xl w-full border-2
+                                focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
                                     Sign up
                                 </Link>
                                 <Link href="register" className="rounded-md bg-gray-500 px-4 py-2 text-sm font-medium md:text-xl w-full
-                    focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
+                                focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
                                     Login
                                 </Link>
                             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-import Image from "next/image";
 import ShowCase from "./components/containers/ShowCase";
 import About1 from "./components/containers/About";
 import Footer from "./components/layout/footer";
+import Header from "./components/containers/Header";
 
 export default function Home() {
   const variants = {
     hidden: { opacity: 0, x: -200, y: 0 },
-    enter: { opacity: 0.8, x: 0, y: 0},
-  
+    enter: { opacity: 1, x: 0, y: 0 },
+
   }
   return (
     <>
-    <main className="flex min-h-screen flex-col items-center justify-between lg:px-24 px-10">
-      <ShowCase title="NOT JUST AN ORDINARY SHELL" description="InMould technology makes the helmet lighter and very firm. This technology is currently the best one for making ultralight helmets and providing you maximum protection. How does InMould work? Expandable polystyrene is injected into our polycarbonate shells under pressure, heat and steam. The inner and outer shell are just one piece, they will not separate, so you can enjoy your ride to the fullest." image="/images/helmet-feature1@2x.png" icon={['/images/icon_pc.svg',"/images/icon_inmold.svg"]} variants={variants}></ShowCase>
-   <About1></About1>
-    </main>
-       <Footer></Footer>
-       </>
+      <Header/>
+
+      <main className="flex min-h-screen flex-col items-center justify-between lg:px-24 px-10">
+        <ShowCase title="NOT JUST AN ORDINARY SHELL" description="InMould technology makes the helmet lighter and very firm. This technology is currently the best one for making ultralight helmets and providing you maximum protection. How does InMould work? Expandable polystyrene is injected into our polycarbonate shells under pressure, heat and steam. The inner and outer shell are just one piece, they will not separate, so you can enjoy your ride to the fullest." image="/images/helmet-feature1@2x.png" icon={['/images/icon_pc.svg', "/images/icon_inmold.svg"]} variants={variants}></ShowCase>
+        <About1></About1>
+      </main>
+      <Footer></Footer>
+    </>
   );
 }
