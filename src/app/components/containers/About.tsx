07207dd9898847c73ca44db193d590262d6a3a22
Skillet-95 <yfeijoo95@gmail.com>
2024-03-11 15:39:40 -0400

    delete extra font-custom

--- src/app/components/containers/About.tsx
+++ src/app/components/containers/About.tsx
@@ -12,7 +12,7 @@ const About1 = () => {
         <div className="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
           <img className="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="https://www.myhqbc.com/images/helmet-feature4.png" alt="blog"/>
           <div className="px-6 py-8">
-            <h4 className="font-custom mt-4 text-2xl font-custom text-neutral-600">
+            <h4 className="font-custom mt-4 text-2xl  text-neutral-600">
               <span className="">Entry</span>
             </h4>
             <p className="font-custom mt-4 text-base font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
@@ -25,7 +25,7 @@ const About1 = () => {
         <div className="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
           <img className="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="https://www.myhqbc.com/images/helmet-feature3.png" alt="blog"/>
           <div className="px-6 py-8">
-            <h4 className="font-custom mt-4 text-2xl font-custom text-neutral-600">
+            <h4 className="mt-4 text-2xl font-custom text-neutral-600">
               <span className="">Entry</span>
             </h4>
             <p className="font-custom mt-4 text-base font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
@@ -38,7 +38,7 @@ const About1 = () => {
         <div className="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
           <img className="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="https://www.myhqbc.com/storage/images/thumb_1564739456_PaulLange_Prilba_09-copy.jpg" alt="blog"/>
           <div className="px-6 py-8">
-            <h4 className="font-custom mt-4 text-2xl font-custom text-neutral-600">
+            <h4 className="font-custom mt-4 text-2xl  text-neutral-600">
               <span className="">Entry</span>
             </h4>
             <p className="font-custom mt-4 text-base font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
@@ -51,7 +51,7 @@ const About1 = () => {
         <div className="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
           <img className="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="https://www.myhqbc.com/storage/images/thumb_1564739456_PaulLange_Prilba_09-copy.jpg" alt="blog"/>
           <div className="px-6 py-8">
-            <h4 className="font-custom mt-4 text-2xl font-custom text-neutral-600">
+            <h4 className="font-custom mt-4 text-2xl  text-neutral-600">
               <span className="">Entry</span>
             </h4>
             <p className="font-custom mt-4 text-base font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
