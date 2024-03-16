35dc9b77621ceb94dc64015629a5661b49512e44
Drakenwolf <hans.haar6@gmail.com>
2024-03-15 21:38:34 -0500

    delete about  peer client request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 
 "use client"
 import ShowCase from "../app/components/containers/ShowCase";
-import About1 from "../app/components/containers/About";
 import Footer from "../app/components/layout/Footer";
 import Header from "../app/components/layout/Header";
 import Details from "../app/components/containers/Details";
@@ -76,7 +75,6 @@ export default function Home() {
         <ShowCase title="NOT JUST AN ORDINARY SHELL" description="InMould technology makes the helmet lighter and very firm. This technology is currently the best one for making ultralight helmets and providing you maximum protection. How does InMould work? Expandable polystyrene is injected into our polycarbonate shells under pressure, heat and steam. The inner and outer shell are just one piece, they will not separate, so you can enjoy your ride to the fullest." image="/images/helmet-feature1@2x.png" icon={['/images/icon_pc.svg', "/images/icon_inmold.svg"]} variants={variants}></ShowCase>
         <ShowCase title="NOT JUST AN ORDINARY SHELL" description="InMould technology makes the helmet lighter and very firm. This technology is currently the best one for making ultralight helmets and providing you maximum protection. How does InMould work? Expandable polystyrene is injected into our polycarbonate shells under pressure, heat and steam. The inner and outer shell are just one piece, they will not separate, so you can enjoy your ride to the fullest." image="/images/helmet-feature1@2x.png" icon={['/images/icon_pc.svg', "/images/icon_inmold.svg"]} variants={variants}></ShowCase>
         <Details product={products} />
-        <About1 />
         <Quote />
 
         <section>
