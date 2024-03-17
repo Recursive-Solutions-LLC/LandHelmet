faa2cf832db75718c742eb1e87e48b21dd81777f
Drakenwolf <hans.haar6@gmail.com>
2024-03-17 14:56:26 -0500

    commmit to avoidmerge overwrite

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -2,10 +2,10 @@ import "../../app/globals.css";
 import Cover from "@/app/components/containers/Cover";
 
 const AboutUs = () => {
- 
+
   return (
     <>
-        < Cover title={"About Us"} image={"/images/about-us.png"}/>
+      < Cover title={"About Us"} srcMobile={"/images/about-us/mobile/cover.png"} srcDesktop={"/images/about-us/desktop/cover.png "} />
     </>
   );
 }
--- src/app/components/containers/Cover.tsx
+++ src/app/components/containers/Cover.tsx
@@ -1,66 +1,42 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Image from "next/image";
+import { useMediaQuery } from 'react-responsive';
 import { CoverProps } from "@/app/interfaces";
 
 const Cover: React.FC<CoverProps> = ({ title, srcDesktop, srcMobile }) => {
-  const [windowWidth, setWindowWidth] = useState<number>(0);
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  const imageSrc = isMobile ? srcMobile : srcDesktop;
 
-  useEffect(() => {
-    const updateWindowDimensions = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", updateWindowDimensions);
-    updateWindowDimensions();
-
-    return () => window.removeEventListener("resize", updateWindowDimensions);
-  }, []);
-  const imageSrc = windowWidth <= 768 ? srcMobile : srcDesktop;
   return (
     <>
-      <div className="z-1   flex justify-center items-center h-96">
-        <h1 className="absolute text-white text-5xl font-bold">{title}</h1>
-
-        <Image
-          src={imageSrc}
-          alt="about-us-cover"
-          width={1500}
-          height={600}
-          className="   object-cover w-screen h-full"
-        />
+      <div className="z-1 flex justify-center items-center h-96">
+        <h1 className="absolute text-5xl font-bold text-white">{title}</h1>
+        <Image src={imageSrc} alt="cover" width={1500} height={600} className="object-cover w-screen h-full" />
       </div>
-      <div className="my-8 lg:mx-24 md:mx-12 sm:mx-12 xs:mx-12 lg:flex md:flex justify-between lg:gap-72 md:gap-16 sm:gap-12 font-custom ">
-        <h2 className="ml-4 font-semibold lg:min-w-[16rem] md:min-w-[14rem] text-black lg:text-4xl text-3xl">
-          Our Talented <br />
-          <strong className="text-field text-black">Leadership</strong> Team
+      <div className="my-8 mx-12 lg:mx-24 flex justify-between gap-12 md:gap-16 lg:gap-72 font-custom">
+        <h2 className="ml-4 text-3xl lg:text-4xl font-semibold text-black">
+          Lorem Ipsum Dolor Sit <br />
+          <strong className="text-field">Amet Consectetur</strong> Adipiscing
         </h2>
-        <div className=" ml-4 mt-4 space-y-2 md:mt-0  text-black text-2xl">
-          <p className="leading-relaxed ">
-            Exceptional thinkers, achievers, collaborators, and trailblazers
-            fuels every success we accomplish.
+        <div className="ml-4 mt-4 md:mt-0 space-y-2 text-2xl text-black">
+          <p className="leading-relaxed">
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
           <a
-            className="text-bold  inline-flex appearance-none items-center py-2 font-medium leading-8 text-field decoration-field underline-offset-8 outline-none transition-all hover:underline focus-visible:underline"
-            href="/careers"
+            className="inline-flex items-center py-2 font-medium leading-8 text-field decoration-field underline-offset-8 outline-none transition-all hover:underline focus-visible:underline"
+            href="/opportunities"
           >
-            Careers at Shepper
+            Explore Opportunities
             <svg
               aria-hidden="true"
               width="1rem"
               height="1rem"
-              focusable="false"
-              data-prefix="fal"
-              data-icon="arrow-right"
-              className="svg-inline--fa fa-arrow-right ml-3"
-              role="img"
+              className="ml-3"
               xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
               viewBox="0 0 448 512"
             >
-              <path
-                fill="currentColor"
-                d="M443.3 267.3c6.2-6.2 6.2-16.4 0-22.6l-176-176c-6.2-6.2-16.4-6.2-22.6 0s-6.2 16.4 0 22.6L393.4 240 16 240c-8.8 0-16 7.2-16 16s7.2 16 16 16l377.4 0L244.7 420.7c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0l176-176z"
-              ></path>
+              <path d="M443.3 267.3c6.2-6.2 6.2-16.4 0-22.6l-176-176c-6.2-6.2-16.4-6.2-22.6 0s-6.2 16.4 0 22.6L393.4 240H16c-8.8 0-16 7.2-16 16s7.2 16 16 16h377.4L244.7 420.7c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0l176-176z" />
             </svg>
           </a>
         </div>
