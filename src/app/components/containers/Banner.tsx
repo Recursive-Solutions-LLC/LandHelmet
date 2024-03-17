d7653e50a60c16c8ad946ad1f5b0436013a5803d
Skillet-95 <yfeijoo95@gmail.com>
2024-03-16 22:32:22 -0400

    fix style banner

--- src/app/components/containers/Banner.tsx
+++ src/app/components/containers/Banner.tsx
@@ -15,18 +15,18 @@ const Banner: React.FC<BannerProps> = ({
 
     const renderSvgs = svgs.map((svg, index) => (
         <div key={index}>
-            <div className="border border-gray-100 p-3 rounded-t-2xl rounded-b-2xl">
-                <Image height={40} width={40} src={svg.svg} alt="icon" className="text-white" />
+            <div className="justify-center flex border border-gray-100 mx-2 p-4 lg:p-7 rounded-3xl ">
+                <Image height={40} width={40} src={svg.svg} alt="icon" className=" text-white" />
             </div>
-            <h1 className="mt-2 text-gray-100 justify-center text-center text-2xl font-bold ">{svg.title}</h1>
+            <h1 className="m-2 text-gray-100 justify-center text-center text-lg font-medium leading-6">{svg.title}</h1>
         </div>
     ))
     
     return (
         <>
-            <div className="lg:flex md:flex sm: z-1 font-custom  m-9 items-center h-96 bg-[#78797e] rounded-rounded-lg rounded-t-3xl rounded-b-3xl">
+            <div className="lg:flex  sm:z-1 font-custom  m-9 items-center h-96 bg-[#78797e] rounded-rounded-lg rounded-t-3xl rounded-b-3xl">
                 <div id="1" className="flex-1 h-60 flex lg:px-24 pd:mx-12 sm:px-12 xs:px-12 items-center justify-center">
-                    <h1 className="text-gray-100 justify-center text-center text-6xl font-bold ">{ReactHtmlParser(title)}</h1>
+                    <h1 className=" text-gray-100 justify-center text-center lg:text-6xl text-3xl font-medium ">{ReactHtmlParser(title)}</h1>
                 </div>
                 <div className="flex-1 flex items-center h-24 justify-center">
                     <div id="2" className=" flex gap-8 justify-center items-center lg:px-24 pd:mx-12 sm:px-12 xs:px-12 ">
